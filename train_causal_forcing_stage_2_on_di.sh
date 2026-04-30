85a81d48a5bae50c72d1372ea522f8a5c6321121
guanyi1119 <guanyi.zhao@qq.com>
2026-04-30 15:44:13 +0800

    change dataset for ODE training

--- train_causal_forcing_stage_2_on_di.sh
+++ train_causal_forcing_stage_2_on_di.sh
@@ -10,7 +10,22 @@ export HCCL_DETERMINISTIC=TRUE
 export HCCL_DATA_PARALLEL_OPTIMIZE=TRUE
 export TASK_QUEUE_ENABLE=1
 
-python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise', '/cache/ODE6KCausal_chunkwise')"
+mkdir /cache/CausalForcingData
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_0', '/cache/CausalForcingData/ODE6KCausal_chunkwise_0')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_1', '/cache/CausalForcingData/ODE6KCausal_chunkwise_1')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_2', '/cache/CausalForcingData/ODE6KCausal_chunkwise_2')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_3', '/cache/CausalForcingData/ODE6KCausal_chunkwise_3')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_4', '/cache/CausalForcingData/ODE6KCausal_chunkwise_4')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_5', '/cache/CausalForcingData/ODE6KCausal_chunkwise_5')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_6', '/cache/CausalForcingData/ODE6KCausal_chunkwise_6')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_7', '/cache/CausalForcingData/ODE6KCausal_chunkwise_7')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_8', '/cache/CausalForcingData/ODE6KCausal_chunkwise_8')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_9', '/cache/CausalForcingData/ODE6KCausal_chunkwise_9')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_10', '/cache/CausalForcingData/ODE6KCausal_chunkwise_10')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_11', '/cache/CausalForcingData/ODE6KCausal_chunkwise_11')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_12', '/cache/CausalForcingData/ODE6KCausal_chunkwise_12')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_13', '/cache/CausalForcingData/ODE6KCausal_chunkwise_13')"
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/CausalForcingData/ODE6KCausal_chunkwise_14', '/cache/CausalForcingData/ODE6KCausal_chunkwise_14')"
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/wan_models/Wan2.1-T2V-1.3B', '/cache/wan_models/Wan2.1-T2V-1.3B')"
 
 MASTER_ADDR=$(echo $VC_WORKER_HOSTS | cut -d',' -f1)
