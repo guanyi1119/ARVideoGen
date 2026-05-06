0c8cb33671bc3914527428ba254bdb4a1aba363d
guanyi1119 <guanyi.zhao@qq.com>
2026-05-06 16:54:16 +0800

    fix bug

--- train_causvid_ode_from_scratch_on_di.sh
+++ train_causvid_ode_from_scratch_on_di.sh
@@ -32,4 +32,4 @@ python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-trai
 MASTER_ADDR=$(echo $VC_WORKER_HOSTS | cut -d',' -f1)
 
 torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_causvid.py \
-    --config_path configs/causvid/causal_ode_init.yaml \
+    --config_path configs/causvid/causal_ode_init.yaml
