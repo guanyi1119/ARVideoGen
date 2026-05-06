0356b555a895b351bd45b6bc4b720b03aa9b7e5c
guanyi1119 <guanyi.zhao@qq.com>
2026-05-06 14:09:03 +0800

    update some scripts

---
+++ train_causvid_ode_from_scratch.sh
@@ -0,0 +1,15 @@
+export MODELS_DIR="/data/z00546255/models/wan_models"
+export OUTPUT_URL="./"
+export DEVICE_TYPE="npu"
+export PYTHONPATH=$PYTHONPATH:"/use/local/Ascend/ascend-toolkit/latest/python/site-packages":"./"
+
+export HCCL_INTER_HCCS_DISABLE=TRUE
+export HCCL_BUFFSIZE=200
+export HCCL_OP_EXPANSION_MODE="AIV"
+
+export HCCL_DETERMINISTIC=TRUE
+export HCCL_DATA_PARALLEL_OPTIMIZE=TRUE
+export TASK_QUEUE_ENABLE=2
+
+torchrun --nproc_per_node=8 train_causvid.py \
+    --config_path configs/causvid/causal_ode_init.yaml
--- train_causvid_ode_from_scratch_on_di.sh
+++ train_causvid_ode_from_scratch_on_di.sh
@@ -31,6 +31,5 @@ python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-trai
 
 MASTER_ADDR=$(echo $VC_WORKER_HOSTS | cut -d',' -f1)
 
-torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_causal_forcing.py \
-    --config_path configs/causal_forcing/causal_ode_chunkwise_from_scratch.yaml \
-    --logdir train_outputs/causal_forcing_ode_chunkwise_from_scratch
+torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_causvid.py \
+    --config_path configs/causvid/causal_ode_init.yaml \
