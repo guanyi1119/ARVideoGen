c8a5b2e45496cd97e01ea471bdd6fb33a830c713
guanyi1119 <guanyi.zhao@qq.com>
2026-06-02 15:15:51 +0800

    fix bug

--- train_causal_forcing_ode_continue_on_di.sh
+++ train_causal_forcing_ode_continue_on_di.sh
@@ -10,9 +10,7 @@ export HCCL_DETERMINISTIC=TRUE
 export HCCL_DATA_PARALLEL_OPTIMIZE=TRUE
 export TASK_QUEUE_ENABLE=1
 
-mkdir /cache/CausalForcingData
-python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/custom_ct_2m_shrink/test_lmdb_ode', '/cache/test_lmdb_ode')"
-
+python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/custom_ct_2m_shrink/test_lmdb_ode', '/cache/lmdb_ode')"
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/wan_models/Wan2.1-T2V-1.3B', '/cache/wan_models/Wan2.1-T2V-1.3B')"
 # AR Diffusion model should be put here: /cache/init_model.pt
 
@@ -21,5 +19,5 @@ MASTER_ADDR=$(echo $VC_WORKER_HOSTS | cut -d',' -f1)
 torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_causal_forcing.py \
     --config_path configs/causal_forcing/causal_ode_chunkwise_continue.yaml \
     --logdir train_outputs/causal_forcing_ode_chunkwise_continue \
-    --data_path /cache/test_lmdb_ode \
-    --data_name_pattern test_lmdb_ode
+    data_path=/cache/lmdb_ode \
+    data_name_pattern=test_lmdb_ode
