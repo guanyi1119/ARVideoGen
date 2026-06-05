0a7f854a6d8f75224af8c85b1167ef809003a1a1
guanyi1119 <guanyi.zhao@qq.com>
2026-06-05 15:01:50 +0800

    fix bug

--- train_reward_forcing_on_di.sh
+++ train_reward_forcing_on_di.sh
@@ -20,6 +20,6 @@ python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-trai
 
 MASTER_ADDR=$(echo $VC_WORKER_HOSTS | cut -d',' -f1)
 
-torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_causal_forcing.py \
+torchrun --nproc_per_node=8 --rdzv_conf="timeout=7200" --nnodes=$VC_WORKER_NUM --node_rank=$VC_TASK_INDEX --master_addr $MASTER_ADDR --master_port 12345 train_reward_forcing.py \
     --config_path configs/reward_forcing/reward_forcing_dmd.yaml \
     --logdir train_outputs/reward_forcing_dmd
