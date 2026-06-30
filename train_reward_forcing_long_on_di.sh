cc2358040867a681dead363f85f36c1d722b2ea1
guanyi1119 <guanyi.zhao@qq.com>
2026-06-30 17:01:19 +0800

    update script for streaming training

--- train_reward_forcing_long_on_di.sh
+++ train_reward_forcing_long_on_di.sh
@@ -13,9 +13,10 @@ export TASK_QUEUE_ENABLE=1
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/wan_models/Wan2.1-T2V-1.3B', '/cache/wan_models/Wan2.1-T2V-1.3B')"
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/wan_models/Wan2.1-T2V-14B', '/cache/wan_models/Wan2.1-T2V-14B')"
 # python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/vidprom_filtered_extended.txt', '/cache/vidprom_filtered_extended.txt')"
-# python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/vidprom_filtered_extended_switch.txt', '/cache/vidprom_filtered_extended_switch.txt')"
-python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/pretrain_30w_15s_20230211_0-5s.txt', '/cache/vidprom_filtered_extended.txt')"
-python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/pretrain_30w_15s_20230211_5-10s.txt', '/cache/vidprom_filtered_extended_switch.txt')"
+python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/vidprom_filtered_extended_llm_scored.jsonl', '/cache/vidprom_filtered_extended_llm_scored.jsonl')"
+python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/vidprom_filtered_extended_switch.txt', '/cache/vidprom_filtered_extended_switch.txt')"
+# python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/pretrain_30w_15s_20230211_0-5s.txt', '/cache/vidprom_filtered_extended.txt')"
+# python -c "import moxing as mox; mox.file.copy('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/data/DMDPrompts/pretrain_30w_15s_20230211_5-10s.txt', '/cache/vidprom_filtered_extended_switch.txt')"
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/VideoReward', '/cache/VideoReward')"
 python -c "import moxing as mox; mox.file.copy_parallel('obs://yw-pixelgeek-training-data-gy1/01.USERS/z00546255/models/Qwen2-VL-2B-Instruct', '/cache/Qwen2-VL-2B-Instruct')"
 
